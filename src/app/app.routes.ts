65cd27716e5816214d2745342b370348a2ccc3cc
Eugene Kasap <eugene.kasap@olx.com>
2025-07-21 17:06:01 +0200

    [feat]: add routing configuration for lazy loading repositories feature

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./features/repositories/repositories.routes').then(
+        (m) => m.repositoriesRoutes,
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
