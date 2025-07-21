59ceb0baa9563e84ef29ba90146edb76f54c3059
Eugene Kasap <eugene.kasap@olx.com>
2025-07-21 17:06:17 +0200

    [feat]: update app layout with router outlet and remove title

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss',
 })
 export class App {}
