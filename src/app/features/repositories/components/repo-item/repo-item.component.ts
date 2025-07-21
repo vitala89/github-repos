8691cccdf8a34c5093b08a8c8e20a898bc06c2bf
Eugene Kasap <eugene.kasap@olx.com>
2025-07-22 01:19:02 +0200

    [feat]: enhance RepoItemComponent to open RepoModal on repository name click

--- src/app/features/repositories/components/repo-item/repo-item.component.ts
+++ src/app/features/repositories/components/repo-item/repo-item.component.ts
@@ -1,13 +1,15 @@
-import { Component, input } from '@angular/core';
+import { Component, input, ViewChild } from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { Repository } from '@features/repositories';
+import { RepoModalComponent } from '@features/repositories/components/repo-modal/repo-modal.component';
 
 @Component({
   selector: 'app-repo-item',
   standalone: true,
-  imports: [CommonModule, NgOptimizedImage],
+  imports: [CommonModule, NgOptimizedImage, RepoModalComponent],
   templateUrl: './repo-item.component.html',
 })
 export class RepoItemComponent {
   repo = input.required<Repository>();
+  @ViewChild(RepoModalComponent) modal!: RepoModalComponent;
 }
