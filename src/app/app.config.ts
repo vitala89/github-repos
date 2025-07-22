b22beae6487fab9efb82ad6bce67f7d2b2e68df9
Eugene Kasap <eugene.kasap@olx.com>
2025-07-22 14:39:41 +0200

    [feat]: configure HTTP client with error interceptor for improved error handling

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,15 +6,16 @@ import {
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideToastr } from 'ngx-toastr';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { httpErrorInterceptor } from '@core/interceptors/http-error.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([httpErrorInterceptor])),
     provideRouter(routes),
     provideAnimations(),
     provideToastr(),
