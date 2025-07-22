88f4878333f4fd35741cfd3ef1fd5b8da8828411
Eugene Kasap <eugene.kasap@olx.com>
2025-07-22 14:20:32 +0200

    [feat]: add module alias for core directory in Jest configuration

--- jest.config.js
+++ jest.config.js
@@ -13,5 +13,6 @@ module.exports = {
     '^@mocks/(.*)$': '<rootDir>/mocks/$1',
     '^@features/(.*)$': '<rootDir>/src/app/features/$1',
     '^@shared/(.*)$': '<rootDir>/src/app/shared/$1',
+    '^@core/(.*)$': '<rootDir>/src/app/core/$1',
   },
 };
