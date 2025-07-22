157ea1c5e61a444ae589d9f9c54cb3c816a0a853
Eugene Kasap <eugene.kasap@olx.com>
2025-07-22 14:20:48 +0200

    [feat]: add Toastr and Animations providers to app configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,6 +7,8 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { provideToastr } from 'ngx-toastr';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,5 +16,7 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideHttpClient(),
     provideRouter(routes),
+    provideAnimations(),
+    provideToastr(),
   ],
 };
