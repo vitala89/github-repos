fa95d42bab9a225cd13d497523ed09ce9bcf294d
Eugene Kasap <eugene.kasap@olx.com>
2025-07-22 16:41:03 +0200

    [feat]: enhance repo item component with lucide-angular icons and improved styling

--- src/app/features/repositories/components/repo-item/repo-item.component.ts
+++ src/app/features/repositories/components/repo-item/repo-item.component.ts
@@ -3,15 +3,33 @@ import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { Repository } from '@features/repositories';
 import { RepoModalComponent } from '@features/repositories/components/repo-modal/repo-modal.component';
 import { RatingStore } from '@features/repositories/stores/rating.store';
+import {
+  BugIcon,
+  LucideAngularModule,
+  StarIcon,
+  UserIcon,
+} from 'lucide-angular';
+import { StarRatingComponent } from '@shared/ui/star-rating/star-rating.component';
 
 @Component({
   selector: 'app-repo-item',
   standalone: true,
-  imports: [CommonModule, NgOptimizedImage, RepoModalComponent],
+  imports: [
+    CommonModule,
+    NgOptimizedImage,
+    RepoModalComponent,
+    LucideAngularModule,
+    StarRatingComponent,
+  ],
   templateUrl: './repo-item.component.html',
 })
 export class RepoItemComponent {
   private readonly ratingStore = inject(RatingStore);
+
+  readonly starIcon = StarIcon;
+  readonly bugIcon = BugIcon;
+  readonly userIcon = UserIcon;
+
   repo = input.required<Repository>();
 
   rating = computed(() => {
