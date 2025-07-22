8e61ceb7ff2ac73b52bef849441445e9ddc59024
Eugene Kasap <eugene.kasap@olx.com>
2025-07-22 23:06:17 +0200

    [feat]: enhance theme toggle button with lucide-angular icons and improved accessibility

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,12 +2,22 @@ import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ThemeService } from '@core/themes/theme.service';
+import {
+  LucideAngularModule,
+  SunIcon,
+  MoonIcon,
+  StarIcon,
+} from 'lucide-angular';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, LucideAngularModule],
   templateUrl: './app.html',
 })
 export class App {
+  readonly sunIcon = SunIcon;
+  readonly moonIcon = MoonIcon;
+
   theme = inject(ThemeService);
+  protected readonly starIcon = StarIcon;
 }
