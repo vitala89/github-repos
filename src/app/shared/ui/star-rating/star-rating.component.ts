e63156a407cd6b8aafdc8a60ab7d6cfbc8c1c0e9
Eugene Kasap <eugene.kasap@olx.com>
2025-07-22 16:40:43 +0200

    [feat]: enhance star rating component with readonly support and lucide-angular icons

--- src/app/shared/ui/star-rating/star-rating.component.ts
+++ src/app/shared/ui/star-rating/star-rating.component.ts
@@ -6,18 +6,23 @@ import {
   OutputEmitterRef,
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LucideAngularModule, StarIcon } from 'lucide-angular';
 
 @Component({
   selector: 'app-star-rating',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, LucideAngularModule],
   templateUrl: './star-rating.component.html',
 })
 export class StarRatingComponent {
   value: InputSignal<number> = input(0);
+  readonly: InputSignal<boolean> = input(false);
   rated: OutputEmitterRef<number> = output<number>();
+  readonly starIcon = StarIcon;
 
   set(val: number): void {
-    this.rated.emit(val);
+    if (!this.readonly()) {
+      this.rated.emit(val);
+    }
   }
 }
